756fc8a7da2faba9f253f9ca10ed4d1a9b779059
Gr8-m8 <martinn.lindbladd@gmail.com>
2022-03-19 11:09:43 +0100

    Add project files.

---
+++ Snake/GameObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class GameObject
+    {
+        int[] Position;
+        public int[] getPosition => Position;
+
+        public GameObject(int[] setPosition)
+        {
+            Position = setPosition;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
